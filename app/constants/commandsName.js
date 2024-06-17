f7ba7bf284067118f813330978d6b06710b0352c
ElBartt <arnauld.alex@gmail.com>
2024-06-17 14:59:38 -0400

    refactor: Remove unused server and user commands
    This commit removes the unused server and user commands from the utility module. These commands were deleted as they were no longer needed in the bot's functionality. Removing them helps to declutter the codebase and improve maintainability.

--- app/constants/commandsName.js
+++ app/constants/commandsName.js
@@ -1,9 +1,8 @@
 module.exports = {
     PING: 'ping',
     CONFIGURE: 'configure',
-    USER: 'user',
-    SERVER: 'server',
     ADD_GAME: 'add-game',
     REMOVE_GAME: 'remove-game',
     GET_GAME: 'get-game',
+    HERE: 'here',
 };
