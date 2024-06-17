3705b45137a4248c6dac0b93dccd0fe94aad2f6f
ElBartt <arnauld.alex@gmail.com>
2024-06-17 14:59:43 -0400

    feat: Add here command to set news channel
    The code changes add a new command called "here" to the configuration module. This command allows users to set the news channel where the bot will spread news. It retrieves the channel ID from the interaction and updates the configuration accordingly. This addition enhances the functionality of the bot by providing a convenient way for users to specify the news channel.

---
+++ app/commands/configuration/here.js
@@ -0,0 +1,27 @@
+const { readConfig, writeConfig } = require('../../config');
+const { SlashCommandBuilder } = require('discord.js');
+const CommandsName = require('../../constants/commandsName');
+const logger = require('../../logger');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName(CommandsName.HERE)
+        .setDescription("Tell the bot to spread news in this channel"),
+    async execute(interaction) {
+        const channelId = interaction.channel.id;
+        const guildId = interaction.guild.id;
+        let config = readConfig();
+
+        let guildConfig = config.guilds.find(g => g.guildId === guildId);
+
+        if (guildConfig) {
+            guildConfig.channelId = channelId;
+        } else {
+            config.guilds.push({ guildId, channelId });
+        }
+
+        writeConfig(config);
+        logger.info(`Channel ${channelId} is now the news channel for guild ${guildId}`);
+        await interaction.reply('I will spread news in this channel');
+    }
+};
