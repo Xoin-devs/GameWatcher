1cf5e45a3d0b77273b7fa40fb29b260381041a1f
ElBartt <arnauld.alex@gmail.com>
2024-07-16 21:58:48 -0400

    chore: Add client cooldowns for command handling

--- app/commandHandler.js
+++ app/commandHandler.js
@@ -5,6 +5,7 @@ const client = require('./client');
 const logger = require('./logger');
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
 
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
--- app/events/interactionCreate.js
+++ app/events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, Collection } = require('discord.js');
 const CustomIds = require('../constants/customIds');
 const logger = require('../logger');
 
@@ -23,6 +23,29 @@ async function handleSlashCommand(interaction) {
         logger.error(`No command matching ${interaction.commandName} was found.`);
         return;
     }
+
+    const { cooldowns } = interaction.client;
+
+    if (!cooldowns.has(command.data.name)) {
+        cooldowns.set(command.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const defaultCooldownDuration = 8;
+    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+    if (timestamps.has(interaction.user.id)) {
+        const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const expiredTimestamp = Math.round(expirationTime / 1_000);
+            return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+        }
+    }
+    
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
     await executeSlashCommand(interaction, command);
 }
@@ -68,8 +91,7 @@ async function handleAutoComplete(interaction) {
 
     try {
         await command.autocomplete(interaction);
-    }
-    catch (error) {
+    } catch (error) {
         logger.error(`Error executing autocomplete for command ${interaction.commandName}: ${error.message}`);
     }
 }
